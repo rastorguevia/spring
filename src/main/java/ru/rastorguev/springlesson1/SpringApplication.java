f26d8da012cbc53be675b15bf02dda5d62b58e6e
rastorguev.i.a <rastorguev.i.a@yandex.ru>
2021-11-20 20:54:48 +0300

    init

---
+++ src/main/java/ru/rastorguev/springlesson1/SpringApplication.java
@@ -0,0 +1,12 @@
+package ru.rastorguev.springlesson1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rastorguev/springlesson1/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rastorguev.springlesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
