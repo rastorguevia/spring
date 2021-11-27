ac1f866b546e5bf46b455b8da3e862d1b8cf74cb
rastorguev.i.a <rastorguev.i.a@yandex.ru>
2021-11-27 20:22:22 +0300

    Исправления.

---
+++ src/main/java/ru/rastorguev/springlesson1/ioc/beanPostProcessors/CacheResultBeanPostProcessor.java
@@ -0,0 +1,39 @@
+package ru.rastorguev.springlesson1.ioc.beanPostProcessors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import ru.rastorguev.springlesson1.ioc.annotations.CacheResult;
+import ru.rastorguev.springlesson1.ioc.beanPostProcessors.methodInterceptor.CacheResultMethodInterceptor;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+//@Component
+@Deprecated
+public class CacheResultBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+        Method[] methods = bean.getClass().getDeclaredMethods();
+        log.info("postProcessAfterInitialization: {}", beanName);
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(CacheResult.class)) {
+                log.info("Bean {} is proxy. Has annotation @CacheResult on method: {}", beanName, method.getName());
+                ProxyFactory proxyFactory = new ProxyFactory(bean);
+                proxyFactory.addAdvice(new CacheResultMethodInterceptor());
+                return proxyFactory.getProxy();
+            }
+        }
+        return bean;
+    }
+
+}
---
+++ src/main/java/ru/rastorguev/springlesson1/ioc/beanPostProcessors/CustomAnnotationBeanPostProcessor.java
@@ -0,0 +1,45 @@
+package ru.rastorguev.springlesson1.ioc.beanPostProcessors;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import ru.rastorguev.springlesson1.ioc.annotations.CacheResult;
+import ru.rastorguev.springlesson1.ioc.beanPostProcessors.methodInterceptor.CacheResultMethodInterceptor;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+@Component
+public class CustomAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    private final Map<Class<? extends Annotation>, MethodInterceptor> methodInterceptorMap = Map.of(
+            CacheResult.class, new CacheResultMethodInterceptor()
+    );
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Method[] declaredMethods = bean.getClass().getDeclaredMethods();
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        for (Method declaredMethod : declaredMethods) {
+            for (Map.Entry<Class<? extends Annotation>, MethodInterceptor> proxyEntry : methodInterceptorMap.entrySet()) {
+                if (declaredMethod.isAnnotationPresent(proxyEntry.getKey())) {
+                    proxyFactory.addAdvice(proxyEntry.getValue());
+                }
+            }
+        }
+
+        if (proxyFactory.getAdvisorCount() > 0) {
+            return proxyFactory.getProxy();
+        }
+
+        return bean;
+    }
+}
