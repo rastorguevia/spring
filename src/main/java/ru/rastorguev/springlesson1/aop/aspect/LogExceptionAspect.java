4c93f93793e72ed6d7c62d76a25847b9f7ff10fa
rastorguev.i.a <rastorguev.i.a@yandex.ru>
2021-12-10 20:10:45 +0300

    hw-2 По заданию было только логировать ошибки.

--- src/main/java/ru/rastorguev/springlesson1/aop/aspect/LogExceptionAspect.java
+++ src/main/java/ru/rastorguev/springlesson1/aop/aspect/LogExceptionAspect.java
@@ -1,7 +1,9 @@
 package ru.rastorguev.springlesson1.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -14,20 +16,20 @@ import static ru.rastorguev.springlesson1.aop.utils.Util.returnRequiredType;
 @Aspect
 public class LogExceptionAspect {
 
-//    @AfterThrowing(value = "execution(* *(..))", throwing = "e")
-//    @AfterThrowing(value = "within(ru.rastorguev..*)", throwing = "e")
-//    public void throwAdvice(JoinPoint jp, Exception e) {
-//        log.error("METHOD {} FINISHED WORK WITH EXCEPTION:", jp.getSignature().toShortString(), e);
-//    }
-
-    @Around("within(ru.rastorguev..*)")
-    public Object throwAdvice(ProceedingJoinPoint jp) {
-        try {
-            return jp.proceed();
-        } catch (Throwable e) {
-            log.error("METHOD {} FINISHED WORK WITH EXCEPTION :", jp.getSignature().toShortString(), e);
-            return returnRequiredType(((MethodSignature) jp.getSignature()).getReturnType());
-        }
+    //@AfterThrowing(value = "execution(* *(..))", throwing = "e")
+    @AfterThrowing(value = "within(ru.rastorguev..*)", throwing = "e")
+    public void throwAdvice(JoinPoint jp, Exception e) {
+        log.error("METHOD {} FINISHED WORK WITH EXCEPTION:", jp.getSignature().toShortString(), e);
     }
+
+//    @Around("within(ru.rastorguev..*)")
+//    public Object throwAdvice(ProceedingJoinPoint jp) {
+//        try {
+//            return jp.proceed();
+//        } catch (Throwable e) {
+//            log.error("METHOD {} FINISHED WORK WITH EXCEPTION :", jp.getSignature().toShortString(), e);
+//            return returnRequiredType(((MethodSignature) jp.getSignature()).getReturnType());
+//        }
+//    }
 
 }
